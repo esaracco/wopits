2deaf80cbdc4dfd2f679da770481319b60726a7b
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-05-14 00:49:25 +0200

    0.4alpha2

--- app/config.php
+++ app/config.php
@@ -5,7 +5,7 @@
       __DIR__.'/../site-config.php');
 
   define ('WPT_DEV_MODE', true);
-  define ('WPT_VERSION', '0.4alpha1');
+  define ('WPT_VERSION', '0.4alpha2');
 
   define ('WPT_COOKIE', 'wopits');
 
