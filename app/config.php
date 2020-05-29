4fb0c0ff3bce58872eadc6fbfe0f3bfdff7d0938
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-05-29 21:35:23 +0200

    0.10alpha1

--- app/config.php
+++ app/config.php
@@ -4,7 +4,7 @@
       __DIR__.'/../site-config.template.php' ://PROD-remove
       __DIR__.'/../site-config.php');
 
-  define ('WPT_VERSION', '0.9alpha1');
+  define ('WPT_VERSION', '0.10alpha1');
 
   define ('WPT_COOKIE', 'wopits');
 
