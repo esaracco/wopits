7649216bb1f21e66790ec9e7f054664e66afd2cc
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-07-03 06:48:36 +0200

    0.16alpha9

--- app/config.php
+++ app/config.php
@@ -6,7 +6,7 @@
     //</WPTPROD-remove>
       __DIR__.'/../site-config.php');
 
-  define ('WPT_VERSION', '0.16alpha8');
+  define ('WPT_VERSION', '0.16alpha9');
   define ('WPT_EXPORT_MIN_VERSION', '0.16');
 
   define ('WPT_COOKIE', 'wopits');
