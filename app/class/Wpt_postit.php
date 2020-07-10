6efb4b37488cf69952a517ed45af01be3782312b
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-07-10 17:19:53 +0200

    Typo.

--- app/class/Wpt_postit.php
+++ app/class/Wpt_postit.php
@@ -170,7 +170,7 @@
             'email' => $item['alert_user_email'],
             'subject' => _("Post-it deadline notification"),
             'msg' => ($days == 1 || ($days == 0 && $hours > 0)) ?
-              sprintf (_("Hello %s,r\n\nThe deadline for the following post-it will expire soon:\n\n%s"), $item['alert_user_fullname'], WPT_URL."/a/w/{$item['wall_id']}/p/{$item['postit_id']}") :
+              sprintf (_("Hello %s,\n\nThe deadline for the following post-it will expire soon:\n\n%s"), $item['alert_user_fullname'], WPT_URL."/a/w/{$item['wall_id']}/p/{$item['postit_id']}") :
               sprintf (_("Hello %s,\n\nThe deadline for the following post-it will expire in %s days:\n\n%s"), $item['alert_user_fullname'], $days, WPT_URL."/a/w/{$item['wall_id']}/p/{$item['postit_id']}")
           ]);
         }
