539f0826e7f715add2a5e36c2ee93f7a3ed9f569
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-07-29 17:46:06 +0200

    0.21alpha3

--- app/config.php
+++ app/config.php
@@ -7,7 +7,7 @@
       __DIR__.'/../site-config.php');
 
   // Version
-  define ('WPT_VERSION', '0.21alpha2');
+  define ('WPT_VERSION', '0.21alpha3');
   define ('WPT_EXPORT_MIN_VERSION', '0.21');
 
   // Cookie
