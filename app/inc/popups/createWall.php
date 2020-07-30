bb1871a60cb83ed06cd8e0a939147c040fd705a4
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-07-30 13:16:02 +0200

    More suitable icon.

--- app/inc/popups/createWall.php
+++ app/inc/popups/createWall.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title"><i class="fas fa-signature fa-lg fa-fw"></i><?=_("New wall")?></h5>
+        <h5 class="modal-title"><i class="fas fa-border-all fa-lg fa-fw"></i><?=_("New wall")?></h5>
         <button type="button" class="close" data-dismiss="modal">
           <span>&times;</span>
         </button>
