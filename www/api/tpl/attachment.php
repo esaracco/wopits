e7912db8e6d605ea22c308384b7fa7bafa11a962
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-08-11 16:59:38 +0200

    Fixed a issue with translation.

--- www/api/tpl/attachment.php
+++ www/api/tpl/attachment.php
@@ -1,4 +1,5 @@
 <?php
+  require_once (__DIR__.'/../../../app/class/Wpt_common.php');
   require_once (__DIR__.'/../../../app/class/Wpt_dbCache.php');
 ?>
 <div id="attachmentPopup" class="modal-collapse">
