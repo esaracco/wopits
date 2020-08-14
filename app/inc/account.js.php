17af1c86d8ff2fa6dda869c2446f2c383a9a057e
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-08-14 14:14:20 +0200

    Wording.

--- app/inc/account.js.php
+++ app/inc/account.js.php
@@ -182,7 +182,7 @@
                   item: $field,
                   placement: "top",
                   title: `<i class="fas fa-eye-slash fa-fw"></i> <?=_("Invisible mode")?>`,
-                  content: "<?=_("Sharing walls will become impossible, and you will be removed from all your current groups.<p/>Other users will also be removed from any groups you created.<p/>Become invisible anyway?")?>",
+                  content: "<?=_("Sharing will be impossible, and you will be removed from all groups.<p/>Become invisible anyway?")?>",
                   cb_close: (btn) => (btn != "yes")&&($field[0].checked= false),
                   cb_ok: () => plugin.updateField ({visible: 0}, true)
                 });
