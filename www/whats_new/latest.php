45f3d47745f791f45b649fd60ec819148c632be7
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-11-08 21:15:23 +0100

    Updated translation.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,4 +1,4 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
-<?=_("<p><b><i class='fas fa-smile-beam  fa-fw'></i>Meta menu</b> You can now clear the selections and menu by pressing <kbd>esc</kbd>. Added <kbd>del</kbd>, <kbd>ctrl+c/x</kbd> aliases for menu options selection, and <kbd>ctrl+v</kbd> will paste notes based on cursor location.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam  fa-fw'></i>Meta menu</b> You can now cancel the selections and close the menu by pressing the esc key. Added aliases <kbd>del</kbd>, <kbd>ctrl+c/x</kbd> to select menu options, and <kbd>ctrl+v</kbd> to paste notes based on cursor position.</p>")?>
 </div>
