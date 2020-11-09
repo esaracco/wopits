803162f923ac189ef11bcc6b8d4e82f19cbc463e
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-11-09 10:27:59 +0100

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,5 +1,4 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Walls sharing</b> Fixed a issue with users search.</p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Meta menu</b> You can now cancel the selections and close the menu by pressing the <kbd>esc</kbd> key. Added aliases <kbd>del</kbd>, <kbd>ctrl+c/x</kbd> to select menu options, and <kbd>ctrl+v</kbd> to paste notes based on cursor position.</p>")?>
+<?=_("<p><b><i class='fas fa-broom fa-fw'></i>Global</b> Wording, cleaning & translation.</p>")?>
 </div>
