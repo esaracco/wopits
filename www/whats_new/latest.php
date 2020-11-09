3703f35f52f321ead903ce39b79d9af5ee34bb99
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-11-09 20:45:15 +0100

    Updated translation.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,5 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p><b><i class='fas fa-broom fa-fw'></i>Global</b> Wording, cleaning & translation.</p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>About</b> Added report/author/support buttons.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>About</b> Added report / author / support buttons.</p>")?>
 </div>
