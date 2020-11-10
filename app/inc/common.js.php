20b19c227259b5464830a20f584381328d1ea7c6
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-11-10 05:50:01 +0100

    Display or not display.

--- app/inc/common.js.php
+++ app/inc/common.js.php
@@ -1733,20 +1733,24 @@ class WHelper
 
         $.get ("/whats_new/latest.php", (d)=>
         {
-          if (<?=WPT_DISPLAY_LATEST_NEWS?> && d)
+          <?php if (WPT_DISPLAY_LATEST_NEWS):?>
+          if (d)
+          {
             d = `<h5 class="mb-3 text-center"><i class="fas fa-bullhorn fa-fw"></i> <?=_("What's new in v%s?")?></h5>${d}<button type="button" class="mt-2 btn btn-secondary btn-xs"><i class="fas fa-scroll"></i> <?=_("See more...")?></button>`.replace("%s", "<?=WPT_VERSION?>");
+          }
           else
             d = "<?=_("Upgrade done. Thank you for using wopits!")?>";
+          <?php else:?>
+            d = "<?=_("Upgrade done. Thank you for using wopits!")?>";
+          <?php endif?>
 
           $popup.find(".modal-body").html (d);
-
-          if (<?=WPT_DISPLAY_LATEST_NEWS?>)
-            $popup.find(".modal-body").find("button")
-              .on("click", function ()
-              {
-                $popup.modal ("hide");
-                H.loadPopup ("userGuide");
-              });
+          $popup.find(".modal-body").find("button")
+            .on("click", function ()
+            {
+              $popup.modal ("hide");
+              H.loadPopup ("userGuide");
+            });
 
           this.openModal ($popup);
         });
