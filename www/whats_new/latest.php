0c776e74e333de7d3b8089f13057eb4f74fc5cf7
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-11-18 15:13:22 +0100

    Updated translation.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,5 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Sticky notes</b> Added some effects on update & delete.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Sticky notes</b> Added some effects on update & delete in sharing mode.</p>")?>
 <?=_("<p><b><i class='fas fa-bug fa-fw'></i>Meta menu</b> Fixed a issue with users group selection</p>")?>
 </div>
