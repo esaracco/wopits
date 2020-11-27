13d2498d408125fa2232ee9e82e402105728d099
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-11-27 20:50:36 +0100

    Updated translation.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,4 +1,4 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Sticky notes</b> Added a progress bar to follow the progress of a task.</p>")?>
+<?=_("<p><b><i class='fas fa-broom fa-fw'></i>Global</b> Cleaning & some fixes.</p>")?>
 </div>
--- www/whats_new/mostRecents.php
+++ www/whats_new/mostRecents.php
@@ -1,5 +1,6 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify">
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Sticky notes</b> Added a progress bar to follow the progress of a task.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Walls</b> Added the ability to move rows and columns using their menu.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Sharing</b> Users can now see in real time who is making changes and on what items.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Meta menu</b> You can now cancel the selections and close the menu by pressing the <kbd>esc</kbd> key. Added aliases <kbd>del</kbd>, <kbd>ctrl+c/x</kbd> to select menu options, and <kbd>ctrl+v</kbd> to paste notes based on cursor position.</p>")?>
