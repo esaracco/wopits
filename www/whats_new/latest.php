2aae570179eadeedd212717f184e6f883377d3b1
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-12-03 18:36:35 +0100

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,7 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs at <a href='mailto:report@wopit.com'>report@wopits.com</a> or using the <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Relationships</b> It is now possible to have finer control over the position of each relationship.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Walls</b> Fixed a issue with elements editing.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Relationships</b> Fixed a issue with relationships creation.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Walls</b> Fixed issue with moving rows/columns.</p>")?>
 </div>
