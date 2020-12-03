5214f3e1392a37952e252c134fa1fad4ad5abf49
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-12-03 06:20:26 +0100

    Added news.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -2,6 +2,6 @@
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs at <a href='mailto:report@wopit.com'>report@wopits.com</a> or using the <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Relationships</b> It is now possible to have finer control over the position of each relationship.</p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Full view</b> More fluidity and consistency.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Sticky notes</b> Fixed a issue with deletion.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Walls</b> Fixed a issue with elements editing.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Relationships</b> Fixed a issue with relationships creation.</p>")?>
 </div>
