2211173ace1b9eae360de026927a1e2720a18c23
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-12-06 10:19:18 +0100

    Updated translation.

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define ('WPT_VERSION', '0.52alpha2');
-define ('WPT_LAST_UPDATE', '2020-12-05');
+define ('WPT_VERSION', '0.52alpha3');
+define ('WPT_LAST_UPDATE', '2020-12-06');
 define ('WPT_DISPLAY_LATEST_NEWS', true);
 define ('WPT_EXPORT_MIN_VERSION', '0.21');
 
