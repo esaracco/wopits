bec1bfab6ea5e4b631c469b0d45ef3ab5677dec5
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-12-09 18:17:28 +0100

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,6 +1,6 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs at <a href='mailto:report@wopit.com'>report@wopits.com</a> or using the <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Notes</b> Fixed issue with deleting notes.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Walls</b> Fixed issue with importing walls.</p>")?>
+<?=_("<p><b><i class='fas fa-broom fa-fw'></i>WebSocket</b> Some WebSocket heartbeat rework. <i>Contact us ASAP if you encounter timeouts problem!</i></p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Walls</b> Fixed issue with cloning walls.</p>")?>
 </div>
