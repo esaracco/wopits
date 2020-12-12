29b6333522773d639e9d1e3378b202532e656402
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-12-12 12:58:01 +0100

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,6 +1,7 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs at <a href='mailto:report@wopit.com'>report@wopits.com</a> or using the <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Global</b> Fixed an issue with session timeout.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Accounts</b> Fixed an issue with accounts creation.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Global</b> Fixed an issue with MS Edge and images uploading.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Users search</b> Show username along with full name.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Network</b> Use <i>fetch()</i> wherever possible.</p>")?>
 </div>
