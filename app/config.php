b1824c07db950054e0ec39d4b3efa7b543732237
Emmanuel Saracco <emmanuel@esaracco.fr>
2020-12-12 13:50:36 +0100

    0.53alpha1

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define ('WPT_VERSION', '0.52alpha9');
-define ('WPT_LAST_UPDATE', '2020-12-10');
+define ('WPT_VERSION', '0.53alpha1');
+define ('WPT_LAST_UPDATE', '2020-12-12');
 define ('WPT_DISPLAY_LATEST_NEWS', true);
 define ('WPT_EXPORT_MIN_VERSION', '0.21');
 
