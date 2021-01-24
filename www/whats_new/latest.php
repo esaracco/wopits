c5ae15d87fcfea595905d6a5f3d0b0dbe2b5c060
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-01-24 06:19:02 +0100

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,6 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
-<?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs at <a href='mailto:report@wopit.com'>report@wopits.com</a> or using the <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-broom fa-fw'></i>Relations</b> Cleaning & optimization.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Notes</b> Fixed some issues with dragging & deletion.</p>")?>
+<?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs: <a href='mailto:report@wopit.com?subject=Bug%20report'>report@wopits.com</a>, <a href='https://twitter.com/wopitsapp' target='_blank'>@wopitsapp</a> or <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Relations</b> Fixed some issues with drop shadow and properties color box.</p>")?>
 </div>
