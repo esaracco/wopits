e259169e1070d7d5d8c49f7d270a47808869ec11
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-04-19 20:46:05 +0200

    News.

--- www/whats_new/mostRecents.php
+++ www/whats_new/mostRecents.php
@@ -1,5 +1,6 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify">
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Email management</b> Better management of unsubscribing to messages sent by wopits.</p>")?>
 <?=_("<p><b><i class='fas fa-bug fa-fw'></i>Relations</b> Fixed some issues with drop shadow and properties color box.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Relations</b> One can now customize each relation line (size, color and so on).</p>")?>
 </div>
