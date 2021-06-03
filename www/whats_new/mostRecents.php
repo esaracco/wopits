64c48523c5348c2f9b94929ea1281078e176356c
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-06-03 18:27:11 +0200

    News.

--- www/whats_new/mostRecents.php
+++ www/whats_new/mostRecents.php
@@ -1,7 +1,7 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify">
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Docker</b> wopits docker is out! Help will be welcome to improve it.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Notes</b> You can manage involved users for notes of shared walls.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Walls</b> More intuitive walls menu.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Walls</b> You can control the visibility of walls headers.</p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Notes</b> You can comment the notes.</p>")?>
 </div>
