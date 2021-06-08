782f52b121bd4b68517257290489aad59c169884
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-06-08 08:23:51 +0200

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,6 +1,6 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs: <a href='mailto:report@wopit.com?subject=Bug%20report'>report@wopits.com</a>, <a href='https://twitter.com/wopitsapp' target='_blank'>@wopitsapp</a> or <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Comments</b> Fixed an issue with notifications links.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Comments</b> Fixed an issue with the auto-completion of usernames.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Account</b> Fixed an issue with account update.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Account</b> Blacklisting of the following email domains: <i>outlook.fr</i>, <i>outlook.com</i>, <i>live.fr</i>, <i>live.com</i>, <i>hotmail.fr</i>, <i>hotmail.com</i>. <u>If your email is using one of them, please update it as soon as possible!</u></p>")?>
 </div>
