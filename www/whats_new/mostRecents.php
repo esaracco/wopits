2ed8f6df46ac7177ba5f2be2813291ee3ec326b6
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-06-28 10:24:04 +0200

    News.

--- www/whats_new/mostRecents.php
+++ www/whats_new/mostRecents.php
@@ -1,7 +1,7 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify">
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Notes</b> Now you can create or copy/paste tables in notes body.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Account</b> Blacklisting of the following email domains: <i>outlook.fr</i>, <i>outlook.com</i>, <i>live.fr</i>, <i>live.com</i>, <i>hotmail.fr</i>, <i>hotmail.com</i>. <u>If your email is using one of them, please update it as soon as possible!</u></p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Docker</b> wopits docker is out! Help will be welcome to improve it.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Notes</b> You can manage involved users for notes of shared walls.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Walls</b> You can control the visibility of walls headers.</p>")?>
 </div>
