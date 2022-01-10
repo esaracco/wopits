6faa0b42d81d3b089e00315de1885f30604014d5
Emmanuel Saracco <emmanuel@esaracco.fr>
2022-01-10 15:24:41 +0100

    News.

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,5 +1,6 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs: <a href='mailto:report@wopit.com?subject=Bug%20report'>report@wopits.com</a>, <a href='https://twitter.com/wopitsapp' target='_blank'>@wopitsapp</a> or <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-broom fa-fw'></i>Global</b> Event management remodeling (WIP), debugging and optimization.</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Global</b> The server hosting wopits has changed causing some disruption. Please excuse us!</p>")?>
+<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Swoole</b> Fixes some Swoole tables access warnings.</p>")?>
 </div>
