613b6c97ce56e5163fbf9489360f81b0c5294da1
Emmanuel Saracco <emmanuel@esaracco.fr>
2022-09-17 19:01:14 +0200

    Update news

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,7 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs: <a href='mailto:report@wopit.com?subject=Bug%20report'>report@wopits.com</a>, <a href='https://twitter.com/wopitsapp' target='_blank'>@wopitsapp</a> or <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Global</b> The server hosting wopits has changed causing some disruption. Please excuse us!</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Swoole</b> Fixes some Swoole tables access warnings.</p>")?>
-<?=_("<p><b><i class='fas fa-bug fa-fw'></i>Notes</b> Fixes a error with color picker.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>LDAP</b> Thanks to Martin Juhl, one can now disabled <i>caseExactMatch</i> and use custom filter for login.</p>")?>
 </div>
