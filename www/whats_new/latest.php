be74b4b606eac812fae33ffbf8ba9d33b63a9eab
Emmanuel Saracco <emmanuel@esaracco.fr>
2022-09-23 16:47:11 +0200

    Update news

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,5 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs: <a href='mailto:report@wopit.com?subject=Bug%20report'>report@wopits.com</a> or <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>LDAP</b> Thanks to Martin Juhl, one can now disabled <i>caseExactMatch</i> and use custom filter for login.</p>")?>
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Global</b> PHP and JavaScript external modules upgrade.</p>")?>
 </div>
--- www/whats_new/mostRecents.php
+++ www/whats_new/mostRecents.php
@@ -1,5 +1,6 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify">
+<?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>LDAP</b> Thanks to Martin Juhl, one can now disabled <i>caseExactMatch</i> and use custom filter for login.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Global</b> Bootstrap 5, debugging and optimization.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Notes</b> Now you can create or copy/paste tables in notes body.</p>")?>
 <?=_("<p><b><i class='fas fa-smile-beam fa-fw'></i>Notes</b> You can manage involved users for notes of shared walls.</p>")?>
