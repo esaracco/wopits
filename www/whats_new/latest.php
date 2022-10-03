c0c418eda1222f720604c8a1e925b96c5da618d8
Emmanuel Saracco <emmanuel@esaracco.fr>
2022-10-03 20:19:37 +0200

    News

--- www/whats_new/latest.php
+++ www/whats_new/latest.php
@@ -1,5 +1,5 @@
 <?php require ($_SERVER['DOCUMENT_ROOT'].'/../app/prepend.php')?>
 <div class="newfeatures justify latest">
 <?=_("<p class='warning'><b><i class='fas fa-exclamation-triangle fa-fw'></i>Warning</b> Please, note that wopits is still in alpha stage! You are warmly invited to report bugs: <a href='mailto:report@wopit.com?subject=Bug%20report'>report@wopits.com</a> or <a href='https://github.com/esaracco/wopits/issues' target='_blank'>GitHub bug tracker</a>.</p>")?>
-<?=_("<p><b><i class='fas fa-broom fa-fw'></i>Global</b> Lot of debugging and cleaning!</p>")?>
+<?=_("<p><b><i class='fas fa-broom fa-fw'></i>Global</b> Continuation of the redesign (cleaning, optimization and formatting).</p>")?>
 </div>
