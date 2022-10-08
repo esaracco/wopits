c97ef3ad64ebb719fea7aa0d7157e2f42565cf2a
Emmanuel Saracco <emmanuel@esaracco.fr>
2022-10-09 01:30:30 +0200

    Fixes WebSocket protocol setting

--- app/inc/main.js.php
+++ app/inc/main.js.php
@@ -2246,7 +2246,7 @@
         setTimeout (()=>{
 
         WS.connect (
-          `<?=WPT_USE_HTTPS ? 'wss' : 'ws'?>://${location.host}/app/ws?token=${wpt_userData.token}`, ()=>
+          `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}/app/ws?token=${wpt_userData.token}`, ()=>
           {
             const $settings = $("#settingsPopup");
 
