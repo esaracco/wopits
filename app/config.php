37ca0c44b68ad9f5f76e97c59e97347d49d77cfb
Emmanuel Saracco <emmanuel@esaracco.fr>
2022-10-20 10:01:07 +0200

    0.65-alpha.7

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('WPT_VERSION', '0.65-alpha.6');
+define('WPT_VERSION', '0.65-alpha.7');
 define('WPT_LAST_UPDATE', '2022-10-20');
 define('WPT_DISPLAY_LATEST_NEWS', false);
 define('WPT_EXPORT_MIN_VERSION', '0.21');
